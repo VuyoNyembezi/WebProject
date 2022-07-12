5cb75ae84e6cecf61d8e7684a13dfb30aa303852
Vuyo Nyembezi <vuyonyembezi513@gmail.com>
2022-07-12 12:49:41 +0200

    email function working

--- WebProject/Controllers/HomeController.cs
+++ WebProject/Controllers/HomeController.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MailKit.Net.Smtp;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using MimeKit;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -12,12 +15,40 @@ namespace WebProject.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private readonly IConfiguration _config;
+        public HomeController(ILogger<HomeController> logger, IConfiguration config)
         {
             _logger = logger;
+            _config = config;
         }
+        [HttpPost]
+        public JsonResult SendEmail(EmailModel request)
+        {
+            if (request.Service != String.Empty && request.Cell != String.Empty)
+            {
+                var email = new MimeMessage();
+                email.From.Add(MailboxAddress.Parse(_config.GetSection("EmailUserName").Value));
+                email.To.Add(MailboxAddress.Parse(_config.GetSection("EmailUserName").Value));
+                email.Subject = request.Service;
+                email.Body = new TextPart(MimeKit.Text.TextFormat.Html) { Text = request.Message + " " +"My Cell: " + request.Cell };
 
+                using var smtp = new SmtpClient();
+                smtp.Connect(_config.GetSection("EmailHost").Value, 587, MailKit.Security.SecureSocketOptions.StartTls);
+                smtp.Authenticate(_config.GetSection("EmailUserName").Value, _config.GetSection("EmailPassword").Value);
+                smtp.Send(email);
+                smtp.Disconnect(true);
+
+                var message = "email sent";
+
+                return Json(message);
+            }
+
+            var failed_message = "error occurd please provide more information";
+            return Json(failed_message);
+          
+
+
+        }
         public IActionResult Index()
         {
             return View();
